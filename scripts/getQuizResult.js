5f088f156dbc42d62d437cea40525b00ebd3bff7
Rachel Suter <rachelsuter@gwu.edu>
2021-11-23 07:55:48 -0500

    Working on getting results for quiz

--- scripts/getQuizResult.js
+++ scripts/getQuizResult.js
@@ -29,7 +29,7 @@ var flowers = [
         "image": ["../images/flowers/carnation-pink-1.png", "../images/flowers/carnation-pink-2.png", "../images/flowers/carnation-pink-3.png"],
         "icon": "../images/flower-icons/carnation-icon-pink.png",
         "meaning": "Pink carnations show gratitude, fondness, and mother-like love.",
-        "tags": "gratitude, fondness, mother"
+        "tags": "gratitude, fondness, mother, thank you"
     },
     {
         "flowerId": 5,
@@ -53,7 +53,7 @@ var flowers = [
         "image": ["../images/flowers/tulip-peach-1.png", "../images/flowers/tulip-peach-2.png", "../images/flowers/tulip-peach-3.png"],
         "icon": "../images/flower-icons/tulip-icon-peach.png",
         "meaning": "Peach tulips symbolize loyalty and gratitude.",
-        "tags": "loyalty, gratitude"
+        "tags": "loyalty, gratitude, thank you"
     },
     {
         "flowerId": 8,
@@ -72,6 +72,7 @@ var flowers = [
         "tags": "cheer, happy"
     }
 ];
+var flowersMatches = [0,0,0,0,0,0,0,0,0,0];
 
 $(document).ready(function(){
     console.log("q1 "+sessionStorage.getItem("q1"));
@@ -80,26 +81,82 @@ $(document).ready(function(){
 
     var q1 = sessionStorage.getItem("q1").toLowerCase();
     var q2 = sessionStorage.getItem("q2").toLowerCase();
-    var q3 = sessionStorage.getItem("q3").toLowerCase().split(",");
+    var q3 = sessionStorage.getItem("q3").toLowerCase().split(", ");
 
     var answers = [q1, q2, q3[0], q3[1], q3[2]];
     console.log(answers);
 
-    var flowersMatches = [0,0,0,0,0,0,0,0,0,0];
 
-    var i = 0;
-    var j = 0;
-    while (i <= answers.length) {
-        while (j <= flowersMatches.length) {
-            console.log(typeof flowers[j].tags);
-            //console.log(typeof answers[i]);
-            console.log(flowers[j].tags);
-            if ((flowers[j+1].tags).includes(answers[i])) {
-                console.log(flowers[j].tags);
-                flowers[j]++;
+ 
+   /* console.log("TEST");
+    console.log(flowers[0].tags);
+    console.log(flowers[1].tags);
+    console.log(flowers[2].tags);
+    console.log(flowers[3].tags);
+    console.log(flowers[4].tags);
+    console.log(flowers[5].tags);
+    console.log(flowers[6].tags);
+    console.log(flowers[7].tags);
+    console.log(flowers[8].tags);
+    //console.log(flowers[9].tags);
+    console.log("TEST");*/
+
+
+   /* var k = 0;
+    while (i < answers.length) {
+        console.log("answers at " +i+ ": " + answers[i]);
+        while (j < flowers.length) {
+
+            console.log("answers: " + answers[k]);
+            if ((flowers[j].tags).includes(answers[k])) {
+                console.log("tags: " + flowers[j].tags);
+                //console.log("answers: " + answers[i]);
+                flowersMatches[j]++;
             }
             j++;
+            k++;
+        }
+        k = 0;
+        i++;
+    }
+    console.log(flowersMatches); */
+   
+    var j = 0;
+
+    while (j < flowers.length) {
+        if (flowers[j].tags.includes(answers[2])) {
+            flowersMatches[j]++;
+        }
+        if (flowers[j].tags.includes(answers[3])) {
+            flowersMatches[j]++;
+        }
+        if (flowers[j].tags.includes(answers[4])) {
+            flowersMatches[j]++;
+        } else {
+
+        }
+        j++;
+    }
+    console.log(flowersMatches);
+
+    var topThree = [0,1,3];
+
+    var i = 0;
+    while (i < flowersMatches.length) {
+        if (flowersMatches[i]>=flowersMatches[topThree[0]]) {
+            topThree[2] = topThree[1];
+            topThree[1] = topThree[0];
+            topThree[0] = i;
         }
         i++;
     }
+    console.log(topThree);
+
+    $('#byb-flower-1').attr('src', flowers[topThree[0]].image[0]);
+    $('#byb-flower-2').attr('src', flowers[topThree[1]].image[1]);
+    $('#byb-flower-3').attr('src', flowers[topThree[2]].image[2]);
+    $('#quiz-meaning-1').html(flowers[topThree[0]].meaning);
+    $('#quiz-meaning-2').html(flowers[topThree[1]].meaning);
+    $('#quiz-meaning-3').html(flowers[topThree[2]].meaning);
+
 });
